739dd330ad3390b14aeb142ff545d1d77d510c01
Taibu Haroub <haroubtaibu@gmail.com>
2021-05-30 00:33:21 +0300

    Add more css and js for form wave animation

---
+++ 09 Form Wave Animation/assests/js/main.js
@@ -0,0 +1,20 @@
+const labels = document.querySelectorAll(".form-control label");
+
+/* labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map((letter, idx) => `<span>${letter}</span>`)
+    .join("");
+});
+ */
+
+//will add the wavy feel
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
